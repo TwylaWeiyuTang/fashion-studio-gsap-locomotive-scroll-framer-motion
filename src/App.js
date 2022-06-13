66ee270cf93eda865968102d7102cead83feae3d
Weiyu Tang <twyla9876@outlook.com>
2022-06-13 19:02:09 +0100

    first draft of the landing page

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+import { ThemeProvider } from 'styled-components';
+import './App.css';
+import GlobalStyle from './styles/GlobalStyle';
+import { dark } from './styles/Themes';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import Home from './Pages/Home';
+import 'locomotive-scroll/dist/locomotive-scroll.css'
+
+function App() {
+  const containerRef = useRef(null)
+
+  return (
+    <>
+    <GlobalStyle />
+
+    <ThemeProvider theme={dark}>
+      <LocomotiveScrollProvider 
+      options={
+        {
+          smooth: true,
+          // ... all available Locomotive Scroll instance options 
+        }
+      }
+      watch={
+        [
+          //..all the dependencies you want to watch to update the scroll.
+          //  Basicaly, you would want to watch page/location changes
+          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+        ]
+      }
+      containerRef={containerRef}>
+        <main data-scroll-container ref={containerRef}>
+         <Home />
+        </main>
+      </LocomotiveScrollProvider>
+    </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
